e15790bb006e021955c9d4146f5fd5e81e1e4b8d
xitornet.23@gmail.com <xitornet.23@gmail.com>
2023-12-26 17:44:07 -0300

    *Style - required_inputs

--- src/pages/customer/register/index.jsx
+++ src/pages/customer/register/index.jsx
@@ -26,6 +26,7 @@ export default function CustomerRegister() {
       Street: street,
       Neighborhood: neighborhood,
       Complement: complement,
+      CurrentDate: currentDate,
     };
 
     console.log(Customer);
@@ -43,38 +44,44 @@ export default function CustomerRegister() {
               <div className="grid grid-cols-2 divide-x-2 divide-gray-600  py-6">
                 {/* COL 01 */}
                 <div className="flex flex-col space-y-4 pr-4">
-                  <label className="font-semibold">Empresa</label>
+                  <label className="flex flex-row font-semibold items-center">Empresa <div className="font-bold text-red-600 pl-1">  *   </div> </label>
                   <input
                     type="text"
-                    className="border-b border-gray-600 outline-none mb-4"
+                    required
+                    className="peer border-b text-sm border-gray-600 outline-none "
                     placeholder="Ex. Assaí atacadista"
                     value={company}
                     onChange={(event) => setCompany(event.target.value)}
                   />
+                  
+                 
 
-                  <label className="font-semibold">Contato</label>
+                  <label className="flex flex-row font-semibold items-center">Contato <div className="font-bold text-red-600  pl-1">  *   </div> </label>
                   <input
                     type="text"
-                    className="border-b border-gray-600 outline-none mb-4"
+                    required
+                    className="border-b text-sm border-gray-600 outline-none mb-4"
                     placeholder="Ex. João Silva"
                     value={contact}
                     onChange={(event) => setContact(event.target.value)}
                   />
 
-                  <label className="font-semibold">E-mail</label>
+                  <label className="flex flex-row font-semibold items-center">E-mail <div className="font-bold text-red-600  pl-1">  *   </div> </label>
                   <input
                     type="email"
-                    className="border-b border-gray-600 outline-none mb-4"
+                    required
+                    className="border-b text-sm border-gray-600 outline-none mb-4"
                     placeholder="Ex. joao.silva@frutacontrol.com"
                     value={email}
                     onChange={(event) => setEmail(event.target.value)}
                   />
 
-                  <label className="font-semibold">Telefone</label>
+                  <label className="flex flex-row font-semibold items-center"> Telefone <div className="font-bold text-red-600  pl-1">  *   </div> </label>
                   <InputMask
                     mask="(99) 99999-9999"
                     type="tel"
-                    className="border-b border-gray-600 outline-none mb-6"
+                    required
+                    className="border-b text-sm border-gray-600 outline-none mb-6"
                     placeholder="Ex. (11) 98765-4321"
                     value={telephone}
                     onChange={(event) => setTelephone(event.target.value)}
@@ -83,24 +90,26 @@ export default function CustomerRegister() {
                 {/* COL 02 */}
                 <div className="flex flex-col space-y-4 pl-4">
 
-                  <label className="font-semibold">Categoria</label>
-                  <select className="border-b border-gray-600 outline-none" value={category} onChange={(event) => setCategory(event.target.value)}>
+                  <label className="flex flex-row font-semibold items-center">Categoria <div className="font-bold text-red-600  pl-1">  *   </div> </label>
+                  <select className="border-b text-sm border-gray-600 outline-none" value={category} required onChange={(event) => setCategory(event.target.value)}>
                     <option value="varejo">Varejo</option>
                     <option value="atacado">Atacado</option>
                   </select>
 
-                  <label className="font-semibold">Rua</label>
+                  <label className="flex flex-row font-semibold items-center">Rua<div className="font-bold text-red-600  pl-1">  *   </div> </label>
                   <InputMask
                     type="text"
-                    className="border-b border-gray-600 outline-none "
+                    required
+                    className="border-b text-sm border-gray-600 outline-none "
                     placeholder="R. Gramado Verde"
                     value={street}
                     onChange={(event) => setStreet(event.target.value)}
                   />
-                  <label className="font-semibold">Bairro</label>
+                  <label className="flex flex-row font-semibold items-center">Bairro<div className="font-bold text-red-600  pl-1">  *   </div> </label>
                   <InputMask
                     type="text"
-                    className="border-b border-gray-600 outline-none mb-2"
+                    required
+                    className="border-b text-sm border-gray-600 outline-none mb-2"
                     placeholder="Ex. Jardim Rio Azul"
                     value={neighborhood}
                     onChange={(event) => setNeighborhood(event.target.value)}
@@ -109,7 +118,7 @@ export default function CustomerRegister() {
                   <label className="font-semibold ">Complemento</label>
                   <input
                     type="text"
-                    className="border-b border-gray-600 outline-none "
+                    className="border-b text-sm border-gray-600 outline-none "
                     placeholder="Ex. Portão amarelo"
                     value={complement}
                     onChange={(event) => setComplement(event.target.value)}
