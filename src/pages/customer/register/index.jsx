86d5812f4e078741a758edf05b812e293476d208
xitornet.23@gmail.com <xitornet.23@gmail.com>
2023-12-26 12:45:21 -0300

    *Build - Adding inputs that make sense

--- src/pages/customer/register/index.jsx
+++ src/pages/customer/register/index.jsx
@@ -1,35 +1,44 @@
 import InputMask from "react-input-mask";
 
 export default function CustomerRegister() {
+  const dateNow = new Date();
+  const year = dateNow.getFullYear();
+  const month = dateNow.getMonth() + 1;
+  const day = dateNow.getDate();
+  const currentDate = `${day}/${month}/${year}`;
+
+
   return (
     <div className="relative pt-8 justify-center flex ">
-      <div className=" bg-white shadow-lg rounded-xl flex ">
+      <div className="bg-white shadow-lg rounded-xl flex">
         <div className="p-10 flex-col divide-y-2 divide-black ">
-          <div className="text-center font-medium text-2xl pb-8 ">[ Clientes ]</div>
+          <div className="text-center font-medium text-2xl pb-8 ">
+            [ Clientes ]
+          </div>
           <div className="flex justify-center ">
             <form>
               <div className="grid grid-cols-2 divide-x-2 divide-gray-600  py-6">
                 {/* COL 01 */}
                 <div className="flex flex-col space-y-4 pr-4">
-                  <label className="font-semibold">Nome</label>
+                  <label className="font-semibold">Empresa</label>
                   <input
                     type="text"
                     className="border-b border-gray-600 outline-none mb-4"
-                    placeholder="Ex. João da Silva"
+                    placeholder="Ex. Assaí atacadista"
                   />
 
-                  <label className="font-semibold">Cargo</label>
+                  <label className="font-semibold">Contato</label>
                   <input
                     type="text"
                     className="border-b border-gray-600 outline-none mb-4"
-                    placeholder="Ex. Gerente de Compras"
+                    placeholder="Ex. João Silva"
                   />
 
                   <label className="font-semibold">E-mail</label>
                   <input
                     type="email"
                     className="border-b border-gray-600 outline-none mb-4"
-                    placeholder="Ex. joao.silva@example.com"
+                    placeholder="Ex. joao.silva@frutacontrol.com"
                   />
 
                   <label className="font-semibold">Telefone</label>
@@ -42,36 +51,30 @@ export default function CustomerRegister() {
                 </div>
                 {/* COL 02 */}
                 <div className="flex flex-col space-y-4 pl-4">
-                  <label className="font-semibold">Código</label>
-                  <InputMask
-                    mask="#99999"
-                    type="text"
-                    className="border-b border-gray-600 outline-none "
-                    placeholder="Ex. #10000"
-                  />
-
-                  <label className="font-semibold">Data de Cadastro</label>
-                  <InputMask
-                    mask="99/99/9999"
-                    maskChar={null}
-                    type="text"
-                    className="border-b border-gray-600 outline-none mb-2"
-                    placeholder="Ex. 14/05/2024"
-                  />
-
-                  <label className="font-semibold ">Limite de Crédito</label>
-                  <input
-                    type="text"
-                    className="border-b border-gray-600 outline-none "  
-                    placeholder="Ex. R$ 10.000,00"
-                  />
-
                   <label className="font-semibold">Categoria</label>
                   <select className="border-b border-gray-600 outline-none ">
                     <option value="varejo">Varejo</option>
                     <option value="atacado">Atacado</option>
-                    <option value="distribuidor">Distribuidor</option>
                   </select>
+                  <label className="font-semibold">Rua</label>
+                  <InputMask
+                    type="text"
+                    className="border-b border-gray-600 outline-none "
+                    placeholder="R. Gramado Verde"
+                  />
+                  <label className="font-semibold">Bairro</label>
+                  <InputMask
+                    type="text"
+                    className="border-b border-gray-600 outline-none mb-2"
+                    placeholder="Ex. Jardim Rio Azul"
+                  />
+
+                  <label className="font-semibold ">Complemento</label>
+                  <input
+                    type="text"
+                    className="border-b border-gray-600 outline-none "
+                    placeholder="Ex. Portão Grande Azul"
+                  />
                 </div>
               </div>
               <button className="w-full bg-blue-400 rounded-lg font-semibold py-2">
