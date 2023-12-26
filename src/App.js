ce083299f9c29c3cda9914863e6567a6b7aad3ba
xitornet.23@gmail.com <xitornet.23@gmail.com>
2023-12-26 10:16:33 -0300

    *Build - init app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">FrutaControl</h1>
+    </div>
+  );
+}
+
+export default App;
